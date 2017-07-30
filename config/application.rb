ae95f01a24146556e8129a3c645f6ea42eeecccd
ken992 <fire09682@gmain.com>
2017-07-30 13:19:54 +0900

    sqliteの時間を標準時間に変更

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,10 @@ Bundler.require(*Rails.groups)
 module ToyApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
