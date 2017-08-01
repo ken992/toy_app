adedbcec57fb43c795613ef6605c8993ea4a5ffd
ken992 <fire09682@gmain.com>
2017-08-01 21:25:01 +0900

    エラー発生。解析したが不明

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,9 @@
 class Micropost < ApplicationRecord
+  belongs_to :user
+  validates :content,length: { 
+    maximum: 14, 
+    too_long: "入力は%{count}文字までです"
+  }, 
+              presence: true
 end
+
